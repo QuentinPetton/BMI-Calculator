654e5672db94088ec0ef8d7bc3d7f5581c18878d
QuentinPetton <quentin.petton@hotmail.fr>
2024-06-27 17:01:40 +0200

    bmi calcul ok with condition

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-//function calcul BMI
 
-function bmiCalculator(weight, height){
-    const bmi = weight / (height * height);
-    return Math.round(bmi);
+ // TODO récupérer la valeur input height
+       const inputHeightElement = document.querySelector("#Height");
+       // TODO récupérer la valeur input weight
+       const inputWeightElement = document.querySelector("#Weight");
+
+const app = {
+    init: function(){
+       //ajout écouteurs d'événements sur les input
+       inputHeightElement.addEventListener('input', app.bmiCalculator);
+       inputWeightElement.addEventListener('input', app.bmiCalculator);
+    },
+
+bmiCalculator: function() {
+
+   //On récupère la valeur de Height et Weight
+    const userHeight = parseFloat(inputHeightElement.value);
+    const userWeight = parseFloat(inputWeightElement.value);
+
+    //condition que les deux inputs soient bien saisis avant de mofifier le texte du score
+    if (userHeight > 0 & userWeight >0) {
+         // TODO faire Résultat BMI = weight / (height²)
+    //todo convertir height en mètre depuis cm
+    const bmiResult = userWeight / ((userHeight/100) * (userHeight/100));
+
+//TODO suivant résultat, envoyer message suivant résultat dans span score + P
+const scoreBmiElement = document.querySelector(".scoreBMI");
+scoreBmiElement.innerHTML = bmiResult;
+    }
+
+},
 }
 
-
-const bmi = bmiCalculator(65,1.8);
-console.log(bmi);
+document.addEventListener('DOMContentLoaded', app.init);
