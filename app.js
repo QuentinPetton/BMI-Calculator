23fe419ceaad1300e528c01bd093aebef69e929e
QuentinPetton <quentin.petton@hotmail.fr>
2024-07-08 21:13:28 +0200

    add obesity range

--- app.js
+++ app.js
@@ -36,11 +36,13 @@ bmiCalculator: function() {
     } else if  (bmiResult >= 18.5 && bmiResult <= 24.9) {
         bmiRange = "normal weight";
         resultMeaning.innerHTML = ("A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.");
-    } else if  (bmiResult > 24.9) {
+    } else if  (bmiResult > 25 && bmiResult <= 24.9) {
         bmiRange = "overweight";
         resultMeaning.innerHTML = ("A BMI between 25 and 29.9 indicates being overweight. This may increase the risk of developing health problems such as heart disease, high blood pressure, and certain cancers. Managing weight through lifestyle changes, including diet modifications and increased physical activity, can help improve overall health.");
+    } else if (bmiResult >= 25) {
+        bmiRange = "obesity";
+        resultMeaning.innerHTML = ("Obesity, indicated by a BMI of 30 or higher, is associated with significantly increased health risks. These risks include heart disease, type 2 diabetes, stroke, and joint problems. Effective management often involves comprehensive lifestyle changes, including dietary adjustments, regular physical activity, and potentially medical intervention under healthcare guidance.");
     }
-
 
     //TODO suivant résultat, envoyer message suivant résultat dans span score + P
     const scoreBmiElement = document.querySelector(".scoreBMI");
